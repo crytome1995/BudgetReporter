4f15c2cdee25aa2f6664a25389efa9039101a002
crytome1995 <ethan.lebioda@gmail.com>
2021-06-06 08:50:33 -0500

    Initial push

---
+++ cmd/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"context"
+
+	"github.com/crytome1995/BudgetReporter/pkg/aws"
+	c "github.com/crytome1995/BudgetReporter/pkg/config"
+	"github.com/crytome1995/BudgetReporter/pkg/date"
+	"github.com/spf13/viper"
+)
+
+type UserNameEvent struct {
+	Username string `json:"userName"`
+}
+
+func reportBudget(userName string) {
+	// set name of config file
+	viper.SetConfigName("config")
+	// set location of config.yaml
+	viper.AddConfigPath(".")
+	// allow for viper to look at env vars
+	viper.AutomaticEnv()
+	// set file type
+	// loading of configuration
+	viper.SetConfigType("yml")
+	var configuration c.Configuration
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("Error reading config file, %s", err)
+	}
+	err := viper.Unmarshal(&configuration)
+	if err != nil {
+		fmt.Printf("Unable to decode into struct, %v", err)
+	}
+	dates := date.GenerateDateRange(time.Now())
+	transactions := aws.QueryTransactionsByDate(configuration.Database.TableName, configuration.Aws.DynamoRegion, userName, dates)
+	fmt.Printf("Transactions: %v", transactions)
+	dateSum := date.Sum(transactions)
+	aws.SendSummary(userName, fmt.Sprintf("Total spent this week $%f", dateSum), configuration.Aws.SnsRegion, configuration.Aws.SnsArn)
+}
+
+func HandleRequest(ctx context.Context, username UserNameEvent) (string, error) {
+	reportBudget(username.Username)
+	return fmt.Sprintf("Ran report for user %s!", username.Username), nil
+}
+func main() {
+
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/crytome1995/BudgetReporter
+
+go 1.14
+
+require (
+	github.com/aws/aws-lambda-go v1.24.0 // indirect
+	github.com/aws/aws-sdk-go v1.38.54
+	github.com/spf13/viper v1.7.1 // indirect
+	golang.org/x/net v0.0.0-20210525063256-abc453219eb5 // indirect
+)
